50459eed3d927154af6a43373a923ac90d0be380
Hugo <cellier.hugo@gmail.com>
2022-05-20 12:36:17 +0200

    Fini
    fin

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -5,10 +5,20 @@ public class SnakeController : MonoBehaviour
 {
     public void OnMove(InputAction.CallbackContext context)
     {
+        Input.GetButton("stop");
+
 
     }
 
+    Vector2 v2 = new Vector2(10, 10);
+    Vector2Int v2i;
 
+    Coroutine Move;
+
+    private int timeBeforeMove;
+
+    private bool IsAlive()
+    {
+
+    } 
 }
-
-
